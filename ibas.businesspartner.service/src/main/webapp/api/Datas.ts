fea658452ea535dd9797db6febbd2fce7fb60880
牛加人等于朱 <niuren.zhu@icloud.com>
2018-01-25 16:14:04 +0800

    添加收付款基于单据功能

--- ibas.businesspartner.service/src/main/webapp/api/Datas.ts
+++ ibas.businesspartner.service/src/main/webapp/api/Datas.ts
@@ -49,7 +49,6 @@ export enum emBusinessPartnerNature {
 }
 /** 业务伙伴类型 */
 export enum emBusinessPartnerType {
-
     /** 客户 */
     CUSTOMER,
     /** 供应商 */
@@ -57,7 +56,6 @@ export enum emBusinessPartnerType {
 }
 /** 性别 */
 export enum emGender {
-
     /** 男 */
     MALE,
     /** 女 */
@@ -179,6 +177,60 @@ export namespace conditions {
             condition.alias = "deleted";
             condition.operation = emConditionOperation.EQUAL;
             condition.value = emYesNo.NO.toString();
+            conditions.add(condition);
+            return conditions;
+        }
+    }
+    export namespace contactperson {
+        export function create(type: emBusinessPartnerType, bpCode: string): List<ICondition> {
+            let condition: ICondition;
+            let conditions: List<ICondition> = new ArrayList<ICondition>();
+            // 类型
+            condition = new Condition();
+            condition.bracketOpen = 1;
+            condition.alias = "ownerType";
+            condition.operation = emConditionOperation.EQUAL;
+            condition.value = type.toString();
+            conditions.add(condition);
+            // 激活的
+            condition = new Condition();
+            condition.alias = "activated";
+            condition.operation = emConditionOperation.EQUAL;
+            condition.value = emYesNo.YES.toString();
+            conditions.add(condition);
+            // 业务伙伴编码
+            condition = new Condition();
+            condition.bracketClose = 1;
+            condition.alias = "businessPartner";
+            condition.operation = emConditionOperation.EQUAL;
+            condition.value = bpCode;
+            conditions.add(condition);
+            return conditions;
+        }
+    }
+    export namespace address {
+        export function create(type: emBusinessPartnerType, bpCode: string): List<ICondition> {
+            let condition: ICondition;
+            let conditions: List<ICondition> = new ArrayList<ICondition>();
+            // 类型
+            condition = new Condition();
+            condition.bracketOpen = 1;
+            condition.alias = "ownerType";
+            condition.operation = emConditionOperation.EQUAL;
+            condition.value = type.toString();
+            conditions.add(condition);
+            // 激活的
+            condition = new Condition();
+            condition.alias = "activated";
+            condition.operation = emConditionOperation.EQUAL;
+            condition.value = emYesNo.YES.toString();
+            conditions.add(condition);
+            // 业务伙伴编码
+            condition = new Condition();
+            condition.bracketClose = 1;
+            condition.alias = "businessPartner";
+            condition.operation = emConditionOperation.EQUAL;
+            condition.value = bpCode;
             conditions.add(condition);
             return conditions;
         }
